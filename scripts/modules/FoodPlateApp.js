15c068456bcd7857eb4c2d4d8a12029f88bcacea
kevinruse <kevin@kevinruse.com>
2020-05-30 12:21:15 -0700

    update to modules

--- scripts/modules/FoodPlateApp.js
+++ scripts/modules/FoodPlateApp.js
@@ -1,26 +1,55 @@
+import { checkUser } from './user-test.js';
 
-const FoodPlateApp = {
-    init: () => {
-        window.addEventListener('load', (event) => {
-            console.info(`event.type is: ${event.type}`)
-            console.info(`FoodPlateApp module loaded`);
-            console.info(`InitMainPage module loaded`);
-            FoodPlateApp.InitUser.checkUser();
-        });
-    }
+// https://medium.com/@petertumulty/avoiding-the-global-scope-with-the-revealing-module-pattern-6796ae7af1b9
+// https://general.support.brightcove.com/developer/concepts-javascript-module-design-pattern.html
+const FoodPlateApp = (function() {
+    let user = 'default user';
+    let req = {};
+    let registered = false;
+    let checkInDate = new Date();
+    let returnDate = new Date();
+    let ageGroup = null;
+    let difference = null;
+    let userCode = null;
+    let age = null;
+    let gender = null;
+    let firstName = null;
+    let minutesDifference = null;
+    let secondsDifference = null;
+    let selectedFood = null;
+    let userIsRegistered = false;
+    let regBtn = document.getElementById('initRegister_btn');
+
+    return { user: user,
+             req: req,
+             registered: registered,
+             checkInDate: checkInDate,
+             returnDate: returnDate,
+             difference: difference,
+             userCode: userCode,
+             gender: gender,
+             firstName: firstName,
+             minutesDifference: minutesDifference,
+             secondsDifference: secondsDifference,
+             selectedFood: selectedFood,
+             userIsRegistered: userIsRegistered,
+             regBtn: regBtn,
+             age: age,
+             ageGroup: ageGroup
+    };
+}());
+
+function init() {
+    window.addEventListener('load', (event) => {
+        console.info(`event.type is: ${event.type}`)
+        console.info(`FoodPlateApp module loaded`);
+        console.info(`InitMainPage module loaded`);
+        checkUser();
+    });
 }
 
-/*
-const FoodPlateApp = (() => {
-    function init() {
-        window.addEventListener('load', (event) => {
-            console.log(`event: ${event.type}`)
-            console.log(`FoodPlateApp module loaded`);
-            FoodPlateApp.InitUser.checkUser();
-        });
-    }
-    return {
-        init : init
-    };
-})();
-*/
+init();
+
+export { FoodPlateApp }
+
+
---
+++ scripts/modules/initUser.js
@@ -0,0 +1,23 @@
+
+function checkUser() {
+    console.log('FoodPlateApp InitUser module loaded');
+    let userIsRegistered;
+    let user;
+    let difference;
+    let lastCheckInDate;
+    let returnDate;
+    const regBtn = document.getElementById('initRegister_btn');
+    if (localStorage.length > 0) {
+        console.log('checkUser() called: user has already registered');
+        userIsRegistered = true;
+        getUser();
+        setInitRegisterBtn();
+    }
+    else {
+        console.log('checkUser() called: user has not registered');
+        userIsRegistered = false;
+        setInitRegisterBtn();
+    }
+}
+
+export { checkUser }
