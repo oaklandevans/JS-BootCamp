f78e56dee01c858bd1e3f4973f8f20e3f2cfb393
kevinruse <kevin@kevinruse.com>
2020-06-04 08:18:10 -0700

    update main.js

--- scripts/modules/main.js
+++ scripts/modules/main.js
@@ -1,33 +1,15 @@
-import { checkUser } from './init-app.js';
 import { User } from './user.js';
+import { checkUser } from './init-app.js';
 import { activateFoodIcons } from './plateManagement.js';
 
 const FoodPlate = (function() {
     let user = new User();
     let checkInDate = new Date();
     let returnDate = new Date();
-    let ageGroup = null;
-    // let difference;
-    let minutesDifference = null;
-    let secondsDifference = null;
-    let selectedFood = null;
-    // let userIsRegistered = false;
-    // DOM Elements
-    const regBtn = document.getElementById('initRegister_btn');
-    const firstnameTI = document.getElementById('firstName');
 
     return { user: user,
              checkInDate: checkInDate,
              returnDate: returnDate,
-             // difference: difference,
-             userCode: User.userCode,
-             minutesDifference: minutesDifference,
-             secondsDifference: secondsDifference,
-             selectedFood: selectedFood,
-             // userIsRegistered: userIsRegistered,
-             regBtn: regBtn,
-             ageGroup: ageGroup,
-             firstnameTI
     };
 }());
 
